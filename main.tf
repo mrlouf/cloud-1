51bf9b119a2f8a9d0e07249458a21d4c6ba87271
Nicolas <ponchon.nicolas@gmail.com>
2025-10-13 22:51:42 +0200

    Terraform now deploys only one EC2 instance, the webserver. Ansible can run all playbooks, deploying the app with only one command. Certificates are now self-signed for demo purposes.

--- main.tf
+++ main.tf
@@ -2,7 +2,9 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+################################
 # -_-_-_- DATA SOURCES -_-_-_- #
+################################
 
 # Alias debian AMI to be reused
 data "aws_ami" "debian" {
@@ -21,79 +23,68 @@ data "aws_ami" "debian" {
   owners = ["136693071363"] # Debian
 }
 
-data "aws_security_group" "ansible-sg" {
-  id = "sg-0a25fc5665b04e27d"
-}
-
-data "aws_security_group" "server-sg" {
-  id = "sg-0ff0aa6d2bd9d10c0"
-}
-
 data "aws_key_pair" "asus" {
   key_name = "Asus"
 }
 
-# -_-_-_- RESOURCES -_-_-_- #
+#################################
+# -_-_-_-_- RESOURCES -_-_-_-_- #
+#################################
 
-resource "tls_private_key" "ansible" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+resource "aws_security_group" "webserv_sg" {
+  name        = "webserv-sg"
+  description = "Allow HTTP, HTTPS, and SSH"
+  vpc_id      = "vpc-05ae2da1e076b6992"
 
-resource "aws_key_pair" "ansible" {
-  key_name   = "ansible-key"
-  public_key = tls_private_key.ansible.public_key_openssh
-}
-
-resource "aws_instance" "ansible_master" {
-  ami                     = data.aws_ami.debian.id
-  instance_type           = "t3.micro"
-  vpc_security_group_ids  = [data.aws_security_group.ansible-sg.id]
-  key_name                = data.aws_key_pair.asus.key_name
-  subnet_id               = "subnet-024953d6418267e70"
-
-  tags = {
-    Name = "tf_ansible_master"
+  ingress {
+    description      = "Allow HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  # Install Ansible on the instance at launch and make it run the playbooks
-  user_data = <<-EOF
-    #!/bin/bash
-    apt update
-    apt install git -y
-    
-    # Injecte la clé Ansible pour que le Master communique avec les serveurs
-    cat > /home/admin/.ssh/id_rsa << 'KEYEOF'
-${tls_private_key.ansible.private_key_pem}
-KEYEOF
-    chmod 600 /home/admin/.ssh/id_rsa
-    chown admin:admin /home/admin/.ssh/id_rsa
-    
-    git clone --branch nponchon https://github.com/mrlouf/cloud-1.git /home/admin/
-    bash /home/admin/install-ansible.sh
-    ansible-playbook -i /home/admin/inventory.yaml /home/admin/main.yaml --tags install
-    ansible-playbook -i /home/admin/inventory.yaml /home/admin/main.yaml --tags deploy
-    EOF
+  ingress {
+    description      = "Allow HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] # Using 0.0.0.0/0 for demo purposes; restrict to local IP for prod would be a good idea
+  }
+
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "tf_webserv_sg"
+  }
 }
 
 resource "aws_instance" "web_server" {
   ami                     = data.aws_ami.debian.id
   instance_type           = "t3.micro"
-  vpc_security_group_ids  = [data.aws_security_group.server-sg.id]
-  key_name                = aws_key_pair.ansible.key_name
+  vpc_security_group_ids  = [resource.aws_security_group.webserv_sg.id]
+  key_name                = data.aws_key_pair.asus.key_name
   subnet_id               = "subnet-029e33fb3e3342fac"
-
-  depends_on              = [aws_key_pair.ansible]
 
   tags = {
     Name = "tf_remote_server"
   }
 }
 
-output "ansible_master_ip" {
-  value = aws_instance.ansible_master.public_dns
-}
-
 output "web_server_ip" {
-  value = aws_instance.web_server.public_dns
+  value = aws_instance.web_server.public_ip
 }
